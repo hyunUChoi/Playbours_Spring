3e2431aae28bb6b05f0366bb33b5aa29adf48051
Hyunwoo Choi <chw64233@gmail.com>
2023-09-10 16:53:03 +0900

    Initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,14 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework:spring-aop:6.0.11")
+    implementation("org.springframework:spring-tx:6.0.11")
+    implementation("org.springframework:spring-web:6.0.11")
+    implementation("org.springframework:spring-core:6.0.11")
+    implementation("org.springframework:spring-expression:6.0.11")
+    implementation("org.springframework:spring-beans:6.0.11")
+    implementation("org.springframework:spring-context:6.0.11")
+    implementation("org.springframework:spring-websocket:6.0.11")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
